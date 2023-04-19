6cff81db5a217386fa64cabbf510ad4f3567e1d9
rehmaund <rehmaund44@gmail.com>
2023-04-19 13:47:55 -0400

    Added update likes, comments, actions taken. Need to increment or decrement in the input. UpdateUser gets passed a user object and replaces the user with that id with the user you are passing it.

--- controllers/users/users-controller.js
+++ controllers/users/users-controller.js
@@ -7,13 +7,10 @@ const UserController = (app) => {
   app.get('/api/users/username/:username', findUserByUsername);
   app.post('/api/users', createUser);
   app.delete('/api/users/:uid', deleteUser);
-  app.put('/api/users/:uid', updateUser);/*
-  app.put('/api/users/:uid/likes', incrementUserLikes);
-  app.put('/api/users/:uid/comments', incrementUserComments);
-  app.put('/api/users/:uid/recommendation', incrementUserRecommendations);
-  app.put('/api/users/:uid/contributions', incrementUserContributions);
-  app.put('/api/users/:uid/actionsTaken', incrementUserActionsTaken);
-  */
+  app.put('/api/users/:uid', updateUser);
+  app.put('/api/users/:uid/likes', updateUserLikes);
+  app.put('/api/users/:uid/comments', updateUserComments);
+  app.put('/api/users/:uid/actionsTaken', updateUserActionsTaken);
 }
 
 const getAllUsers = async (req, res) => {
@@ -55,56 +52,26 @@ const updateUser = async (req, res) => {
   const status = await usersDao.updateUser(userId, updates);
   res.json(status);
 }
-/*
-const incrementUserLikes = (req, res) => {
+
+const updateUserLikes = async (req, res) => {
   const userId = req.params.uid;
-  const user = users
-  .find(u => u._id === userId);
-  if (user.type === "ALIEN") {
-    user.total_likes += 1;
-  }
-  res.json(user);
+  const newLikesNum = req.body;
+  const status = await usersDao.updateLikes(userId, newLikesNum);
+  res.json(status);
 }
 
-const incrementUserComments = (req, res) => {
+const updateUserComments = async (req, res) => {
   const userId = req.params.uid;
-  const user = users
-  .find(u => u._id === userId);
-  if (user.type === "ALIEN") {
-    user.total_comments += 1;
-  }
-  res.json(user);
+  const newCommentsNum = req.body;
+  const status = await usersDao.updateComments(userId, newCommentsNum);
+  res.json(status);
 }
 
-const incrementUserRecommendations = (req, res) => {
+const updateUserActionsTaken = async (req, res) => {
   const userId = req.params.uid;
-  const user = users
-  .find(u => u._id === userId);
-  if (user.type === "HUMAN") {
-    user.total_recommendations += 1;
-  }
-  res.json(user);
+  const newActionsNum = req.body;
+  const status = await usersDao.updateActions(userId, newActionsNum);
+  res.json(status);
 }
 
-const incrementUserContributions = (req, res) => {
-  const userId = req.params.uid;
-  const user = users
-  .find(u => u._id === userId);
-  if (user.type === "HUMAN") {
-    user.total_contributions += 1;
-  }
-  res.json(user);
-}
-
-const incrementUserActionsTaken = (req, res) => {
-  const userId = req.params.uid;
-  const user = users
-  .find(u => u._id === userId);
-  if (user.type === "MODERATOR") {
-    user.total_actions_taken += 1;
-  }
-  res.json(user);
-}
-
-*/
 export default UserController;
--- controllers/users/users-dao.js
+++ controllers/users/users-dao.js
@@ -22,4 +22,8 @@ export const createUser = (user) => usersModel.create(user);
 export const deleteUser = (uid) => usersModel.deleteOne({_id: uid});
 export const updateUser = (uid, user) => usersModel.updateOne({_id: uid}, user, { new: true })
 
-export const incrementLikes = (uid) => usersModel.updateOne({_id: uid}, {$set: total_likes})
+export const updateLikes = (uid, newLikes) => usersModel.updateOne({_id: uid}, {$set: {total_likes: newLikes}})
+
+export const updateComments = (uid, newComments) => usersModel.updateOne({_id: uid}, {$set: {total_comments: newComments}})
+
+export const updateActions = (uid, newActions) => usersModel.updateOne({_id: uid}, {$set: {actions_taken: newActions}})
